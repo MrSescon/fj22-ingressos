cf2b515c9b1a016824e237927dbf9077434f8c07
Magno Sescon de Farias Albuqerque <magnosescon@gmail.com>
2019-07-24 15:56:17 -0300

    criando logica para tela de selecao de lugares

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -1,6 +1,7 @@
 package br.com.caelum.ingresso.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,8 +18,10 @@ import org.springframework.web.servlet.ModelAndView;
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.ImagemCapa;
 import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.form.SessaoForm;
+import br.com.caelum.ingresso.rest.OmdbClient;
 import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
 
 @Controller
@@ -31,6 +35,9 @@ public class SessaoController {
 
 	@Autowired
 	private FilmeDao filmeDao;
+	
+	@Autowired
+	private OmdbClient client;
 
 	// O paremetro form se refere aos dados já preenchidos no caso de erro de
 	// validação
@@ -48,28 +55,45 @@ public class SessaoController {
 	}
 
 	@PostMapping("/admin/sessao")
-	@Transactional // para que so ocorra a transacao se nao der erro, somente grava os dados se tudo estiver certo
+	@Transactional // para que so ocorra a transacao se nao der erro, somente grava os dados se
+					// tudo estiver certo
 	public ModelAndView salva(@Valid SessaoForm form, BindingResult result) {
-		
-		// o parametro form é passado para o metodo form acima para que os dados validos digitados pelo usuario permaneçam na view
-		if (result.hasErrors())	return form(form.getSalaId(), form);
-		
+
+		// o parametro form é passado para o metodo form acima para que os dados validos
+		// digitados pelo usuario permaneçam na view
+		if (result.hasErrors())
+			return form(form.getSalaId(), form);
+
 		// validando dados atraves do metodo ToSessao da classe SessaoForm
 		Sessao sessao = form.toSessao(salaDao, filmeDao);
-		
-		//Criando uma lista com as sessoes da sala
+
+		// Criando uma lista com as sessoes da sala
 		List<Sessao> sessoesDaSala = sessaoDao.buscaSessoesDaSala(sessao.getSala());
-		
-		//instanciando o gerenciador/lógica de validaçáo da sessao
+
+		// instanciando o gerenciador/lógica de validaçáo da sessao
 		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoesDaSala);
-		
-		//Gerenciando as sessoes verificando se elas acabam no mesmo dia e se nao estáo se encavalando
-		if(gerenciador.cabe(sessao)) {
+
+		// Gerenciando as sessoes verificando se elas acabam no mesmo dia e se nao estáo
+		// se encavalando
+		if (gerenciador.cabe(sessao)) {
 			sessaoDao.save(sessao);
 			return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
 		}
-		//Caso náo seja validada a sesao, retorna a view passando como parametro o form preenchido com dados que estáo corretos
-		return form(form.getSalaId(), form); 
+		// Caso náo seja validada a sesao, retorna a view passando como parametro o form
+		// preenchido com dados que estáo corretos
+		return form(form.getSalaId(), form);
+	}
+
+	@GetMapping("/sessao/{id}/lugares")
+	public ModelAndView lugaresNaSessao(@PathVariable("id") Integer sessaoId) {
+		ModelAndView modelAndView = new ModelAndView("sessao/lugares");
+		Sessao sessao = sessaoDao.findOne(sessaoId);
+		
+		Optional<ImagemCapa> imagemCapa = client.request(sessao.getFilme(), ImagemCapa.class);
+
+		modelAndView.addObject("sessao", sessao);
+		modelAndView.addObject("imagemCapa", imagemCapa.orElse(new ImagemCapa()));
+		return modelAndView;
 	}
 
 }
