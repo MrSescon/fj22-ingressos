e08308f4cf2481d396fd5c87733ea7bb202a2f07
Magno Sescon de Farias Albuqerque <magnosescon@gmail.com>
2019-07-24 11:02:55 -0300

    adicionado o @before para evitar repeticao de codigo

--- src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import java.time.LocalTime;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.caelum.ingresso.model.Filme;
@@ -12,16 +13,24 @@ import br.com.caelum.ingresso.model.Ingresso;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
 
-
 public class DescontoTest {
-
+	private Sala eldorado;
+	private Filme rogueone;
+	private Sessao sessaoDasDez;
+	
+	
+	@Before
+	public void preparaDescontos() {
+		this.eldorado = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
+		this.rogueone = new Filme("Rogue one", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
+		this.sessaoDasDez = new Sessao(LocalTime.parse("10:00:00"), rogueone, eldorado);
+		
+	}
+	
 	@Test
 	public void naoDeveConcederDescontoParaIngressoNormal() {
-
-		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
-		Filme filme = new Filme("Rogue one", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
-		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
-		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
+		
+		Ingresso ingresso = new Ingresso(sessaoDasDez, new SemDesconto());
 		
 		BigDecimal precoEsperado = new BigDecimal("32.50");
 		
@@ -31,10 +40,7 @@ public class DescontoTest {
 	@Test
 	public void deveConcederDescontoDe30PorcentoParaIngressoClienteDeBancos() {
 		
-		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
-		Filme filme = new Filme("Rogue one", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
-		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
+		Ingresso ingresso = new Ingresso(sessaoDasDez, new DescontoParaBancos());
 		
 		BigDecimal precoEsperado = new BigDecimal("22.75");
 		
@@ -45,10 +51,8 @@ public class DescontoTest {
 	@Test
 	public void deveConcederDescontoDe50PorcentoParaIngressoDeEstudante() {
 		
-		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
-		Filme filme = new Filme("Rogue one", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
-		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
+		
+		Ingresso ingresso = new Ingresso(sessaoDasDez, new DescontoParaEstudantes());
 		
 		BigDecimal precoEsperado = new BigDecimal("16.25");
 		
